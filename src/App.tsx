d435dcfd67d76e60d2cc28c8b998a29eb15bd1a0
priyanshu sharma <pinuwork@gmail.com>
2024-12-11 16:37:41 +0530

    added logic for previous month dates

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import { useState } from 'react'
 import './App.css'
+import { getDaysInAMonth,getFirstDayOfMonth , getMonthArray} from './utils/Calander'
 
 function App() {
 
+  // console.log(getDaysInAMonth(2024,11));
+  // console.log(getFirstDayOfMonth(2024,11));
+  
+  console.log(getMonthArray(2024, 11));
+  
+  
   return (
     <h1>hello world </h1>
   )
