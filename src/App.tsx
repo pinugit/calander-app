60e824b44173db0e58f2aec423034e570c7d07f3
priyanshu sharma <pinuwork@gmail.com>
2024-12-11 20:12:19 +0530

    added buttons to change month

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import './App.css'
-import { CalenderView } from './components/CalenderView'
-import { ModeToggle } from './components/mode-toggle'
-import { ThemeProvider } from './components/theme-provider'
-import { Button } from './components/ui/button'
+import "./App.css";
+import { CalenderView } from "./components/CalenderView";
+import { ModeToggle } from "./components/mode-toggle";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
-
-  return (
-    <ThemeProvider> 
-    <div className='h-screen w-screen flex justify-center items-center bg-zinc-900'>
-    <ModeToggle/> 
-      <CalenderView/></div>
-    </ThemeProvider>
-  )
+	return (
+		<ThemeProvider>
+			<div className="h-screen w-screen flex justify-center items-center bg-zinc-900">
+				<div className="absolute top-3 right-3">
+					<ModeToggle />
+				</div>
+				<CalenderView />
+			</div>
+		</ThemeProvider>
+	);
 }
 
-export default App
+export default App;
--- src/components/CalenderView.tsx
+++ src/components/CalenderView.tsx
@@ -1,20 +1,80 @@
 import { getMonthArray } from "@/utils/Calender";
 import { useState } from "react";
-import { Card, CardTitle } from "./ui/card";
+import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { Button } from "./ui/button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
-export const CalenderView = () =>{
-  const todayDate = new Date().toLocaleDateString().split(
-  "/"
-  );
-  const [currentDate, setCurrentDate] = useState<{date:number,month:number,year:number}>({date:Number(todayDate[0]), month:Number(todayDate[1]), year:Number(todayDate[2])});
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+];
 
-  const [currentMonthArray, setCurrentMonthArray] = useState(getMonthArray(currentDate.year, currentDate.month -1 ))
-  console.log(currentMonthArray);
-  
-  return(
-    <Card >
-      <CardTitle>december</CardTitle>
-    </Card>
-  )
-  
-}
+export const CalenderView = () => {
+	const todayDate = new Date().toLocaleDateString().split("/");
+	const [currentDate, setCurrentDate] = useState<{
+		date: number;
+		month: number;
+		year: number;
+	}>({
+		date: Number(todayDate[0]),
+		month: Number(todayDate[1]),
+		year: Number(todayDate[2]),
+	});
+
+	const [currentMonthArray, setCurrentMonthArray] = useState(
+		getMonthArray(currentDate.year, currentDate.month - 1),
+	);
+	console.log(currentMonthArray);
+
+	const nextMonth = () => {
+		setCurrentDate((prevDate) => ({
+			...prevDate,
+			month: (prevDate.month + 1) % 12,
+			year: prevDate.month === 11 ? prevDate.year + 1 : prevDate.year,
+		}));
+	};
+
+	const prevMonth = () => {
+		setCurrentDate((prevDate) => ({
+			...prevDate,
+			month: (prevDate.month - 1 + 12) % 12,
+			year: prevDate.month === 0 ? prevDate.year - 1 : prevDate.year,
+		}));
+	};
+
+	return (
+		<Card className="h-[650px] w-[600px] p-4">
+			<CardHeader className="flex flex-row justify-between">
+				<div>
+					<CardTitle className="text-3xl">
+						{months[currentDate.month]}
+					</CardTitle>
+					<CardDescription className="text-2xl">
+						{currentDate.year}
+					</CardDescription>
+				</div>
+				<div className="flex gap-2 justify-center items-center">
+					<Button onClick={prevMonth}>
+						{" "}
+						<ChevronLeft />
+						Prev
+					</Button>
+					<Button onClick={nextMonth}>
+						<ChevronRight />
+						Next
+					</Button>
+				</div>
+			</CardHeader>
+		</Card>
+	);
+};
